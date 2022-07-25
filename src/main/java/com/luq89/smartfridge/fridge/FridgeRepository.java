cc32fd816c8d3278d500586c6926749b89e868aa
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2022-07-25 20:44:21 +0200

    fridge repository changes

--- src/main/java/com/luq89/smartfridge/fridge/FridgeRepository.java
+++ src/main/java/com/luq89/smartfridge/fridge/FridgeRepository.java
@@ -1,9 +1,8 @@
 package com.luq89.smartfridge.fridge;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FridgeRepository extends CrudRepository<Fridge, Long> {
-
+public interface FridgeRepository extends JpaRepository<Fridge, Long> {
 }
