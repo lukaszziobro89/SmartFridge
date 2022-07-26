29aba9d0653f856427e82061aea59dc2bb3cbc7c
lukaszziobro89 <lukasz.ziobro.89@gmail.com>
2022-07-26 22:53:04 +0200

    mapper bean added

--- src/main/java/com/luq89/smartfridge/SmartFridgeApplication.java
+++ src/main/java/com/luq89/smartfridge/SmartFridgeApplication.java
@@ -1,5 +1,6 @@
 package com.luq89.smartfridge;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -30,5 +31,10 @@ public class SmartFridgeApplication {
         UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
         urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
         return new CorsFilter(urlBasedCorsConfigurationSource);
+    }
+
+    @Bean
+    public ModelMapper modelMapper(){
+        return new ModelMapper();
     }
 }
